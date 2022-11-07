cb078f3c63ef56d9d1f927bff5871732eaa2bb79
Daniel Badra <danielbadra17@gmail.com>
2022-11-07 11:58:20 -0300

    Exercicios 1-5.1

---
+++ Fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,10 @@
+document.getElementById("page-title").innerText = "Game of Thrones";
+
+document.getElementById("second-paragraph").innerText = "Personagens da Série";
+
+const subtitle = document.getElementById("subtitle");
+subtitle.innerText = "História da Série";
+subtitle.style.padding = "20px";
+
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
